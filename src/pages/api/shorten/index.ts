1dd979c5a4936270d0f413bc5b71622399db306f
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-06-25 17:44:22 +0100

    added zod

--- src/pages/api/shorten/index.ts
+++ src/pages/api/shorten/index.ts
@@ -1,12 +1,24 @@
 import { nanoid } from "nanoid";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../db/client";
+import { z } from "zod";
+
+const Link = z.object({
+  url: z.string(),
+  expirationTime: z.string(),
+});
 
 export default async function shorten(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const body = req.body;
+
+  if (!Link.safeParse(body).success) {
+    res
+      .status(404)
+      .send(JSON.stringify({ message: "invalid url or time type" }));
+  }
 
   if (body.url == null) {
     res.status(404).send(JSON.stringify({ message: "url is required" }));
